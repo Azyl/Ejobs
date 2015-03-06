9d2018db7b044b41689d42ef7fd02a3ef6bc60e7
AndreiTataru <tataru.andrei.emanuel@gmail.com>
2015-03-06 20:27:17 +0200

    working on the data transformer and master data set-up added output sql scripts for the master data

---
+++ Ejobs/dataTransform/OutputSqls/T_careerLevel.sql
@@ -0,0 +1,6 @@
+insert into T_careerLevel(careerLevelId,careerLevelName) values (0,q'!Any career level!');
+insert into T_careerLevel(careerLevelId,careerLevelName) values (9,q'!Unskilled!');
+insert into T_careerLevel(careerLevelId,careerLevelName) values (2,q'!Student!');
+insert into T_careerLevel(careerLevelId,careerLevelName) values (3,q'!Entry-Level!');
+insert into T_careerLevel(careerLevelId,careerLevelName) values (4,q'!Mid-Level!');
+insert into T_careerLevel(careerLevelId,careerLevelName) values (5,q'!Management!');
---
+++ Ejobs/dataTransform/OutputSqls/T_jobType.sql
@@ -0,0 +1,3 @@
+insert into T_jobType(jobAdTypeId,jobAdTypeName) values (q'!0!',q'!Full Time!');
+insert into T_jobType(jobAdTypeId,jobAdTypeName) values (q'!1!',q'!Part Time!');
+insert into T_jobType(jobAdTypeId,jobAdTypeName) values (q'!2!',q'!Project based!');
