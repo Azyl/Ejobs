7b756eae6ebd35405e9a12908d5e61148d248f79
AndreiTataru <tataru.andrei.emanuel@gmail.com>
2015-03-07 20:38:23 +0200

    oracle schema bmp added

---
+++ Ejobs/dataTransform/OutputSqls/installMasterdata.sql
@@ -0,0 +1,12 @@
+@T_country.sql
+@T_county.sql
+@T_city.sql
+@T_industry.sql
+@T_driverLicence.sql
+@T_careerLevel.sql
+@T_language.sql
+@T_jobType.sql
+@T_departments.sql
+
+--verify the output and issue the commit or rollback
+-- depency https://github.com/pljson/pljson
